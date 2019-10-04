8c9be35e3693cf2c83e5bd2311f57fa3d1e380fb
antlikelion <ignorant_woo@naver.com>
2019-10-05 01:56:11 +0900

    class + generics

--- src/practice.ts
+++ src/practice.ts
@@ -1,29 +1,28 @@
-type Person = {
-    name: string,
-    age?:number
-    // 물음표는 설정을 해도 되고 안 해도 되는 값을 의미함
+class Queue<T> {
+    list: T[] = [];
+    get length(){
+        return this.list.length
+    }
+    enqueue(item: T){
+        this.list.push(item)
+    }
+    dequeue(){
+        return this.list.shift()
+        // shift()는 배열의 첫번째 원소를 제거하는 메소드임
+    }
 }
 
-// & 는 Intersection 으로서 두개 이상의 타입들을 합쳐줍니다.
-// 참고: https://www.typescriptlang.org/docs/handbook/advanced-types.html#intersection-types
-type Developer = Person & {
-    skills:string[]
-}
+const queue = new Queue<number>();
+queue.enqueue(0)
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+queue.enqueue(4)
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
 
-const person: Person = {
-    name:"이인우",
-    age: 28
-}
 
-const expert: Developer = {
-    name:"홍길동",
-    skills: ['javascript','react']
-}
 
-type People = Person[]
-// Person[]이라는 type을 People이라는 타입으로 사용할 수 있게 됨
-const people: People = [person, expert];
-
-type Color = 'red' | 'orange' | 'yellow'
-const color: Color = 'red'
-const colors: Color[] = ['red', 'orange']
