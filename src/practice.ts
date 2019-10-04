f4c186a37587d4c00740e18b35774aa2dcdaa74c
antlikelion <ignorant_woo@naver.com>
2019-10-05 01:47:52 +0900

    interface의 대체재인 type!

--- src/practice.ts
+++ src/practice.ts
@@ -1,10 +1,12 @@
-interface Person {
+type Person = {
     name: string,
     age?:number
     // 물음표는 설정을 해도 되고 안 해도 되는 값을 의미함
 }
 
-interface Developer extends Person {
+// & 는 Intersection 으로서 두개 이상의 타입들을 합쳐줍니다.
+// 참고: https://www.typescriptlang.org/docs/handbook/advanced-types.html#intersection-types
+type Developer = Person & {
     skills:string[]
 }
 
@@ -18,4 +20,10 @@ const expert: Developer = {
     skills: ['javascript','react']
 }
 
-const people: Person[] = [person, expert]
+type People = Person[]
+// Person[]이라는 type을 People이라는 타입으로 사용할 수 있게 됨
+const people: People = [person, expert];
+
+type Color = 'red' | 'orange' | 'yellow'
+const color: Color = 'red'
+const colors: Color[] = ['red', 'orange']
