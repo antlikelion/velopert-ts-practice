aeee1a73992687880fc2d019ac71018809f4a6c3
antlikelion <ignorant_woo@naver.com>
2019-10-05 01:42:43 +0900

    extends를 이용한 interface의 상속

--- src/practice.ts
+++ src/practice.ts
@@ -4,9 +4,7 @@ interface Person {
     // 물음표는 설정을 해도 되고 안 해도 되는 값을 의미함
 }
 
-interface Developer {
-    name: string,
-    age?:number,
+interface Developer extends Person {
     skills:string[]
 }
 
@@ -19,3 +17,5 @@ const expert: Developer = {
     name:"홍길동",
     skills: ['javascript','react']
 }
+
+const people: Person[] = [person, expert]
